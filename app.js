c261be69deb084ddb7025bf79e54433e13c96ff8
Majzik "Mini Pablo" Bence <majben686@hengersor.hu>
2024-12-19 11:00:58 +0100

    karacsonyifeladat day 2

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+let szavazatok = Array(12).fill(0);  
+
 function szavazas(cardId) {
-    
     var counterElement = document.getElementById('counter-' + cardId);
     var currentCount = parseInt(counterElement.innerText);
-    counterElement.innerText = currentCount + 1 + "szavazat";
-  }
+    szavazatok[cardId - 1]++;
+    counterElement.innerText = szavazatok[cardId - 1] + " szavazat"; 
+    
+    frissitLegtobbSzavazatotKapott();
+}
+
+function frissitLegtobbSzavazatotKapott() {
+
+    let maxSzavazat = Math.max(...szavazatok);
+    let legnépszerűbbAjándékIndex = szavazatok.indexOf(maxSzavazat);
+    
+    let legnépszerűbbCard = document.getElementById('card-' + (legnépszerűbbAjándékIndex + 1));
+    let legnépszerűbbTitle = legnépszerűbbCard.querySelector('.card-title');
+    
+    let infoBox = document.getElementById('legnépszerűbb-ajándék');
+    infoBox.innerHTML = "A legnépszerűbb ajándék: " + legnépszerűbbTitle.innerText;
+}
